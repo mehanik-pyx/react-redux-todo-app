9ff0c83a0716b526486802989c717fb154cfc83c
Alexander Popov <mehanik-pyx@mail.ru>
2022-03-30 18:16:26 +0700

    add store and todoSlice

---
+++ starter/src/redux/todoSlice.js
@@ -0,0 +1,15 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+    name: "todos",
+    initialState: [
+        { id: 1, title: 'todo1', completed: false },
+        { id: 2, title: 'todo2', completed: false },
+        { id: 3, title: 'todo3', completed: true },
+    ],
+    reducers: {
+        addTodo: (state, action) => {
+            
+        }
+    }
+})
